cb87da5a2619d78b0fb4a85bec0e002d757f9455
Bartosz Sobieski <bartosz.sobieski.1998@wp.pl>
2020-03-11 20:17:42 +0100

    init commit

---
+++ src/main/java/pl/bsobieski/crudlibrary/CrudlibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.bsobieski.crudlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudlibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudlibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bsobieski/crudlibrary/CrudlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bsobieski.crudlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudlibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
