eef3c8f06b88c6f92768ec5bab334d3a82f322dd
Kamil Piskorz <kamilu977@gmail.com>
2020-03-30 14:43:34 +0200

    add jwtfilter

--- src/main/java/pl/bsobieski/crudlibrary/security/WebSecurityConfiguration.java
+++ src/main/java/pl/bsobieski/crudlibrary/security/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -22,7 +23,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
-                .and()
-                .addFilter(jwtFilter);
+                .and();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
