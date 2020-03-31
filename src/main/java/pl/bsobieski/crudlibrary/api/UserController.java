35d1e24417cb2c567b6e86bf96ff5306d750e5bd
Bartosz Sobieski <bartosz.sobieski.1998@wp.pl>
2020-03-31 12:38:23 +0200

    Init commit

--- src/main/java/pl/bsobieski/crudlibrary/api/UserController.java
+++ src/main/java/pl/bsobieski/crudlibrary/api/UserController.java
@@ -1,11 +1,15 @@
 package pl.bsobieski.crudlibrary.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.bsobieski.crudlibrary.entities.User;
 import pl.bsobieski.crudlibrary.services.UserService;
+
+import java.util.Optional;
 //TODO login
 //TODO Registration
 //TODO JWT
@@ -24,5 +28,8 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-
+    @PostMapping("/register")
+    public ResponseEntity registration(){
+        return null;
+    }
 }
